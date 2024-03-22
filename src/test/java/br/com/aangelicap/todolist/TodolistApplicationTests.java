67fcfd1ecb168ea92bbffce0dfe3db93356bc791
Angelica Pelizza <angelicapelizza@hotmail.com>
2024-03-21 23:21:20 -0300

    create project structure using mvn

---
+++ src/test/java/br/com/aangelicap/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aangelicap.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
